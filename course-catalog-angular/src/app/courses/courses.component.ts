593981d46ef5ba144a1291126412a6758b4277ef
nurhofemdiaz <nurhofemdiaz@gmail.com>
2018-04-01 22:19:17 +0200

    Paginador completado

--- course-catalog-angular/src/app/courses/courses.component.ts
+++ course-catalog-angular/src/app/courses/courses.component.ts
@@ -3,6 +3,7 @@ import { COURSES } from '../mocks/mock-courses';
 import { PAGE } from '../mocks/mock-pages';
 
 import { CourseBusinessService } from '../service/course-business-service';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -22,15 +23,57 @@ export class CoursesComponent implements OnInit {
 
     // Load number maximun of pages
     this.courseBusinessService.getPages().subscribe(res =>{
-        this.page.maxNumberPages = res;
+        this.page.maxNumberPages = res._body;
     });
 
     // Load course table
+    this.reloadTable();
+
+  }
+
+  private reloadTable(){
     this.courseBusinessService.getCourses(this.page.pageNumber,this.page.isAscendingOrder).subscribe(res =>{
         this.courses = res.json();
     });
   }
 
+  pressButtonBack() {
+    if(this.page.pageNumber > 0){
+      // Load course table
+      this.page.pageNumber--;
+      this.reloadTable();
+    }
+  }
 
+  pressButtonNext() {
+      if(this.page.pageNumber < this.page.maxNumberPages){
+        // Load course table
+        this.page.pageNumber++;
+        this.reloadTable();
+      }
+  }
 
+  pressButton2() {
+      if(this.page.pageNumber <= this.page.maxNumberPages){
+        // Load course table
+        this.page.pageNumber = (  this.page.pageNumber >= 1) ?  this.page.pageNumber + 1 : 1;
+        this.reloadTable();
+      }
+  }
+
+  pressButton1() {
+      if(this.page.pageNumber <= this.page.maxNumberPages){
+        // Load course table
+        this.page.pageNumber = (this.page.pageNumber > 0) ? this.page.pageNumber - 1 : 0;
+        this.reloadTable();
+      }
+  }
+
+  pressButton3() {
+      if(this.page.pageNumber <= this.page.maxNumberPages){
+        // Load course table
+        this.page.pageNumber = (this.page.pageNumber > 1) ? this.page.pageNumber + 1 : 3;
+        this.reloadTable();
+      }
+  }
 }
